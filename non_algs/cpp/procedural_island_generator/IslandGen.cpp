2551712aebe7a973aec0635b39ab9d262ac6ee92
Craig Grande <c.grande@cox.net>
2025-04-02 17:04:09 -0700

    final implementation of proc island generator
    only outputs to a terminal, using black and white text characters

--- non_algs/cpp/procedural_island_generator/IslandGen.cpp
+++ non_algs/cpp/procedural_island_generator/IslandGen.cpp
@@ -8,73 +8,85 @@ using namespace std;
 int** makeParticleMap(int, int, int, int, int, int, int, int);
 bool moveExists(int**, int, int, int, int, int);
 int findMax(int**, int, int);
-void normalizeMap(int**, int, int, int);
+void normalizeMap(int**, int, int, int, int);
+void clearIntArray(int**, int);
+void clearChaArray(char**, int);
 
 int main(int argc, char** argv) {
   int width, height, windowX, windowY, winWidth, winHeight, numParticles,
       maxLife, waterLine;
+  char presetVal;
+  bool preset;
+
+  // use preset or not
+  do {
+    cout << "Do you wish to use a preset? y or n: ";
+    cin >> presetVal;
+  } while (!(presetVal == 'y' || presetVal == 'Y' || presetVal == 'n' ||
+             presetVal == 'N'));
+
+  if (presetVal == 'y' || presetVal == 'Y')
+    preset = true;
+  else
+    preset = false;
 
   // get user input
-  do {
-    cout << "To begin, enter map width: ";
-    cin >> width;
-    cout << "now enter height: ";
-    cin >> height;
-  } while (!(width > 0 && height > 0));
+  if (preset) {
+    width = 50, height = 50, windowX = 20, windowY = 20, winWidth = 10, winHeight
+    = 10, numParticles = 50, maxLife = 500, waterLine = 50;
+  } else {
+    do {
+      cout << "To begin, enter map width: ";
+      cin >> width;
+      cout << "now enter height: ";
+      cin >> height;
+    } while (!(width > 0 && height > 0));
 
-  do {
-    cout << "Enter drop-window x-coordinate (0 - " << width << "): ";
-    cin >> windowX;
-    cout << "Enter drop-window y-coordinate (0 - " << height << "): ";
-    cin >> windowY;
-    cout << "Enter drop-window width (>= 1): ";
-    cin >> winWidth;
-    cout << "Enter drop-window height (>= 1): ";
-    cin >> winHeight;
-  } while (
-      !((windowX >= 0 && windowY >= 0) &&
-        (windowX <= width && windowY <= height) &&
-        (winWidth > 0 && winHeight > 0) &&
-        ((winWidth + windowX) <= width && (winHeight + windowY) <= height)));
+    do {
+      cout << "Enter drop-window x-coordinate (0 - " << width << "): ";
+      cin >> windowX;
+      cout << "Enter drop-window y-coordinate (0 - " << height << "): ";
+      cin >> windowY;
+      cout << "Enter drop-window width (>= 1): ";
+      cin >> winWidth;
+      cout << "Enter drop-window height (>= 1): ";
+      cin >> winHeight;
+    } while (
+        !((windowX >= 0 && windowY >= 0) &&
+          (windowX <= width && windowY <= height) &&
+          (winWidth > 0 && winHeight > 0) &&
+          ((winWidth + windowX) <= width && (winHeight + windowY) <= height)));
 
-  do {
-    cout << "Enter number of particles to drop: ";
-    cin >> numParticles;
-  } while (!(numParticles > 0));
+    do {
+      cout << "Enter number of particles to drop: ";
+      cin >> numParticles;
+    } while (!(numParticles > 0));
 
-  do {
-    cout << "Enter max life of particles: ";
-    cin >> maxLife;
-  } while (!(maxLife > 0));
+    do {
+      cout << "Enter max life of particles: ";
+      cin >> maxLife;
+    } while (!(maxLife > 0));
 
-  do {
-    cout << "Enter value for waterline (40-200): ";
-    cin >> waterLine;
-  } while (!(waterLine >= 40 && waterLine <= 200));
-
-  // testing
-  /* width = 5, height = 5, windowX = 0, windowY = 0, winWidth = 5, winHeight = 5,
-  numParticles = 4, maxLife = 4, waterLine = 45; */
+    do {
+      cout << "Enter value for waterline (40-200): ";
+      cin >> waterLine;
+    } while (!(waterLine >= 40 && waterLine <= 200));
+  }
 
   // generate map
   int** map = makeParticleMap(width, height, windowX, windowY, winWidth,
                               winHeight, numParticles, maxLife);
 
-  // print map
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-      cout << map[i][j];
-    }
-    cout << endl;
-  }
+  // find max
+  int max = findMax(map, width, height);
+
+  // normalize map and print
+  normalizeMap(map, width, height, max, waterLine);
 }
 
 /*
  * This function builds the array for the map data and performs particle roll
- * algorithm to populate the array with map data. You can modifuy this function
- * to take the array as a parameter instead if you prefer to create the array
- * externally to the particle roll algorithm. In which case, this function would
- * become a void type.
+ * algorithm to populate the array with map data.
  */
 int** makeParticleMap(int width, int height, int windowX, int windowY,
                       int winWidth, int winHeight, int numParticles,
@@ -93,7 +105,7 @@ int** makeParticleMap(int width, int height, int windowX, int windowY,
     map[particleY][particleX]++;
 
     // repeat until current particle dies
-    for (int j = 1; j < maxLife; j++) {
+    for (int j = 0; j < maxLife; j++) {
       // pick a move at random, see if it is a valid move
       // if not valid, pick another random move until out of moves
       int moves[8] = {0, 1, 2, 3, 4, 5, 6, 7};
@@ -106,7 +118,7 @@ int** makeParticleMap(int width, int height, int windowX, int windowY,
 
       for (int j = 0; j < 8; j++) {
         if (moveExists(map, width, height, particleX, particleY, moves[j])) {
-          // update x,y coordinate
+          // update x,y coordinate based on move
           switch (moves[j]) {
             case 0:
               particleY--;
@@ -154,8 +166,8 @@ int** makeParticleMap(int width, int height, int windowX, int windowY,
 }
 
 /*
- * This function looks at the 8 spots around x, y, and determines if a valid
- * move is possible. Return true if one is found.
+ * This function takes in a move to one of the 8 spots aroudn x, y, and
+ * determines if a valid move is possible. Return true if move is valid.
  */
 bool moveExists(int** map, int width, int height, int x, int y, int move) {
   switch (move) {
@@ -207,11 +219,82 @@ bool moveExists(int** map, int width, int height, int x, int y, int move) {
 /*
  * This function finds the max valud in the map and returns it
  */
-int findMax(int** map, int width, int height) { return 0; }
+int findMax(int** map, int width, int height) {
+  int max = 0;
+  for (int i = 0; i < height; i++)
+    for (int j = 0; j < width; j++)
+      if (map[i][j] > max) max = map[i][j];
+
+  return max;
+}
 
 /*
  * Performs the normalization operation on the map data. You could have this
  * return a new array instead if you want to hold onto the og map data for some
  * reason.
  */
-void normalizeMap(int** map, int width, int height, int maxVal) {}
+void normalizeMap(int** map, int width, int height, int maxVal, int waterLine) {
+  // normalize the map to be constant values between 0 - 255
+  // divide each value by the largest, and multiply by 255
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      map[i][j] = (double)map[i][j] / (double)maxVal * 255;
+    }
+  }
+
+  // calculate land-zone
+  int landzone = 255 - waterLine;
+
+  // output array using char instead of numbers
+  char** output = new char*[height];
+  for (int i = 0; i < height; i++) output[i] = new char[width];
+
+  // fill in output array based on the numbers from map
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      int temp = map[i][j];
+      if (temp <= floor(landzone / 2))
+        output[i][j] = '#';  // deep water
+      else if (temp <= waterLine)
+        output[i][j] = '~';  // shallow water
+      else if (temp < (waterLine + ((landzone * 15) / 100)))
+        output[i][j] = '.';  // coast/beach
+      else if (temp < (waterLine + ((landzone * 40) / 100)))
+        output[i][j] = '-';  // plains/grass
+      else if (temp < (waterLine + ((landzone * 80) / 100)))
+        output[i][j] = '*';  // forests
+      else
+        output[i][j] = '^';  // mountains
+    }
+  }
+
+  // print results
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      // printf("\033[97m%c", output[i][j]);
+      // cout << map[i][j];
+      printf("%c ", output[i][j]);
+    }
+    cout << endl;
+  }
+
+  // clear memory
+  clearIntArray(map, height);
+  clearChaArray(output, height);
+}
+
+/*
+ * Deletes content from a double int array to free up memory.
+ */
+void clearIntArray(int** array, int height) {
+  for (int i = 0; i < height; i++) delete[] array[i];
+  delete[] array;
+}
+
+/*
+ * Deletes content from a double char array to free up memory.
+ */
+void clearChaArray(char** array, int height) {
+  for (int i = 0; i < height; i++) delete[] array[i];
+  delete[] array;
+}
